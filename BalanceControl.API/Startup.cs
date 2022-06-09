66e1f6a9c8477493602d5e949c39a7d4e7282852
nuciko24 <nuciko24@list.ru>
2022-06-10 00:16:01 +0400

    Referenced BalanceManager.dll and using Swagger into Startup.cs

--- BalanceControl.API/Startup.cs
+++ BalanceControl.API/Startup.cs
@@ -1,8 +1,11 @@
+using Balances;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace BalanceControl.API
 {
@@ -17,6 +20,32 @@ namespace BalanceControl.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IBalanceManager, CasinoBalanceManager>();
+            services.AddTransient<IBalanceManager, GameBalanceManager>();
+
+            services.AddHttpContextAccessor();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Cors",
+                    builder =>
+
+                    {
+                        builder
+                        .WithOrigins(Configuration.GetSection("Cors:Origins").Get<string[]>())
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                    });
+            });
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BalanceControl.API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -25,6 +54,16 @@ namespace BalanceControl.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseRouting();
+
+            app.UseCors("Cors");
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "BalanceControl.API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
