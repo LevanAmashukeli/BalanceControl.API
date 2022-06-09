b67ccce03e672b560b06ca1307c76d9b90006409
nuciko24 <nuciko24@list.ru>
2022-06-10 00:18:17 +0400

    Call methods into controller by using BalanceManager

--- BalanceControl.API/Controllers/BalanceController.cs
+++ BalanceControl.API/Controllers/BalanceController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Balances;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BalanceControl.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
     public class BalanceController : ControllerBase
     {
+        private readonly IBalanceManager _balanceManager;
+
+        public BalanceController(IBalanceManager balanceManager)
+        {
+            _balanceManager = balanceManager;
+        }
+
+        [HttpPost("balance")]
+        public decimal GetBalance() => _balanceManager.GetBalance();
+
+        [HttpPost("withdraw/{{transactionid}}/{{amount}}")]
+        public ErrorCode DecreaseBalance(decimal amount, string transactioId) => _balanceManager.DecreaseBalance(amount, transactioId);
+
+        [HttpPost("deposit/{{transactionid}}/{{amount}}")]
+        public ErrorCode IncreaseBalance(decimal amount, string transactioId) => _balanceManager.IncreaseBalance(amount, transactioId);
     }
 }
