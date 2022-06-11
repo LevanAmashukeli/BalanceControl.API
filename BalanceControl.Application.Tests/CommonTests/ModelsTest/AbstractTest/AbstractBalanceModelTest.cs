77ab467ce568b22b51a3bc710a5a4a16f88d5503
nuciko24 <nuciko24@list.ru>
2022-06-11 17:56:42 +0400

    Corrected folder division

--- BalanceControl.Application.Tests/CommonTests/ModelsTest/AbstractTest/AbstractBalanceModelTest.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace BalanceControl.Application.Tests.CommonTests.ModelsTest.AbstractTest
-{
-    public class AbstractBalanceModelTest
-    {
-        public decimal Amount { get; set; }
-        public string TransactioId { get; set; }
-    }
-}
--- BalanceControl.Application.Tests/CommonTests/ModelsTest/BalanceTest/DepositBalanceChangeModelTest.cs
+++
@@ -1,8 +0,0 @@
-﻿using BalanceControl.Application.Tests.CommonTests.ModelsTest.AbstractTest;
-
-namespace BalanceControl.Application.Tests.CommonTests.ModelsTest.BalanceTest
-{
-    public class DepositBalanceChangeModelTest : AbstractBalanceModelTest
-    {
-    }
-}
